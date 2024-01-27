4a540332d8a57961a361b8c1c3d98e26e964e578
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-28 05:58:03 +0800

    update herE

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ let fname = window.prompt("What is your name? ") ;
 
 fname = String(fname) ;
 
-window.alert("Hello " + fname + " Welcome to my World!") ;
+window.alert("Hello " + fname + " Welcome to my World!" + " But!") ;
 
 let lname = window.prompt("Can you tell me what is your Lastname ?")
 
 lname = String(fname) ;
+
+window.alert("Thank you for your Participation " + fname + lname + "Welcome!" )
