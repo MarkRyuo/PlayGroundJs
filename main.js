ab7a113f312fecd6fbf31ad8464b889610e5df1e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-28 06:18:13 +0800

    udpate herE

--- main.js
+++ main.js
@@ -15,25 +15,25 @@
 
 // Todo Lets create a simple Math 
 
-let x ;
-let y ;
-let z ;
+// let x ;
+// let y ;
+// let z ;
 
-x = 45 // I have 45 eggs but we need to buy again we need 50 eggs 
-x = Number(x) ;
+// x = 45 // I have 45 eggs but we need to buy again we need 50 eggs 
+// x = Number(x) ;
 
-y = 50
-y = Number(y) ; 
-
-
-z = x + y ;
-// z = x - y ;
-// z = x * y ;
-// z = x / y ;  // 0.9 total here
-
-// z = Math.round(z) ;
-// z = Math.floor(z) ; //  Round Down but pag negative lumabas iraraund up niya ito 
-// z = Math.ceil(z) ; 
+// y = 50
+// y = Number(y) ; 
 
 
-console.log(z + " total of Eggs") ;
+// z = x + y ;
+// // z = x - y ;
+// // z = x * y ;
+// // z = x / y ;  // 0.9 total here
+
+// // z = Math.round(z) ;
+// // z = Math.floor(z) ; //  Round Down but pag negative lumabas iraraund up niya ito 
+// // z = Math.ceil(z) ; 
+
+
+// console.log(z + " total of Eggs") ;
