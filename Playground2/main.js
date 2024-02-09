46c7fc646b504db55809b9d3c0eacfbac552d686
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-09 15:04:08 +0800

    update herE

--- Playground2/main.js
+++ Playground2/main.js
@@ -1,10 +1,10 @@
-// Todo create a hypotenuse 
+// // Todo create a hypotenuse 
 
-let x = 5 ;
-let y = 10 ;
-let z ;
+// let x = 5 ;
+// let y = 10 ;
+// let z ;
 
-z = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) ;
-z = Math.ceil(z) ;
+// z = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) ;
+// z = Math.ceil(z) ;
 
-console.log(z) ;
+// console.log(z) ;
