924e56465f1179b071a6013505aa6aa29656deca
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-11 20:12:29 +0800

    udpate herE

--- Playground2/main.js
+++ Playground2/main.js
@@ -132,6 +132,8 @@ else {
 
 switch(true) {
 
-  case sideA >= 20  
+  case sideA >= 20 && sideB <= 20 :
+    sideC = Math.ceil(Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2))) ;
+    console.log("SideA = " + sideA ) 
 
 }
