4e82342b2f729b1d9a2e0935a2e4ff3b75e2ef0b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-11 09:24:41 +0800

    update herE

--- Playground2/main.js
+++ Playground2/main.js
@@ -117,7 +117,7 @@ let sideC ;
 // todo If statement first 
 
 if (age == 20) {
-  nameDeveloper = nameDeveloper.toUpperCase() ; // kapag naman equal to 20 ito ay i uuppercase ang pangalan 
+  nameDeveloper = nameDeveloper.toUpperCase().slice(0, 4) // kapag naman equal to 20 ito ay i uuppercase ang pangalan 
   console.log(nameDeveloper + " Your old enough welcome to our system")
 
 }
