cce7e22dda8b6c2777773e97d6bdb4d37b0f32e1
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-13 08:08:51 +0800

    update herE

--- Playg3js/play3.js
+++ Playg3js/play3.js
@@ -2,6 +2,19 @@
 
 
 
+// let temp = "pizza" ;
+// let sunny = true ;
+
+// if (temp >= 15 ) {
+//   console.log("Its hot outside") ;
+// }
+// else if (temp >= 5 ) {
+//   console.log("Its cold outside")
+// }
+// else {
+//   console.log(temp + " is not a number") ;
+// }
+
 let temp = "pizza" ;
 let sunny = true ;
 
