ffa3416bdf9452fd9305d1065caeed8e644c5f12
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-13 08:06:48 +0800

    update herE

--- Playg3js/play3.js
+++ Playg3js/play3.js
@@ -8,6 +8,9 @@ let sunny = true ;
 if (temp >= 15 ) {
   console.log("Its hot outside") ;
 }
+else if (temp >= 5 ) {
+  console.log("Its cold outside")
+}
 else {
   console.log("Its cold outside") ;
 }
