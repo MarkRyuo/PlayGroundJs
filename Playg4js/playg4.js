74392ed8854d539d0a2769dd48930382ceab500e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-15 17:16:08 +0800

    udpate herE

--- Playg4js/playg4.js
+++ Playg4js/playg4.js
@@ -5,11 +5,22 @@
 // todo While Loop = repeat some code while some condition is true potentially Infinite 
 
 
+// let userName = "" ;
+
+// while(userName == "") {
+//   userName = "Jhon Mark"
+// }
+
+// console.log("Hello " + userName) ;
+
 let userName = "" ;
 
-while(userName == "") {
+ do {
   userName = "Jhon Mark"
-}
+} while(userName == "")
 
-console.log("Hello " + userName)
+console.log("Hello " + userName) ;
+
+
+
 
