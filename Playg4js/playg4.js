0d078d9120dfc0ffe2807c69d37fbe375f3e76b9
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-17 07:36:52 +0800

    update herE

--- Playg4js/playg4.js
+++ Playg4js/playg4.js
@@ -105,11 +105,15 @@
 
 // ang nilalaman ng i is 20 tapos ang condition ay kung ang i ay is greater than sa 0 ang i is babawasan ng isa 
 
-for (let i = 20 ; i > 0 ; i -=1 ) {
-  if (i == 13 ) {
-    continue ; // lilikdangan niya lamang ang 13 
-  }
-  console.log(i)
-}
+// for (let i = 20 ; i > 0 ; i -=1 ) {
+//   if (i == 13 ) {
+//     continue ; // lilikdangan niya lamang ang 13 
+//   }
+//   console.log(i)
+// }
 
+let age ;
 
+do{
+  console.log("Welcome to adult hood")
+}while(age > 17 ) ;
