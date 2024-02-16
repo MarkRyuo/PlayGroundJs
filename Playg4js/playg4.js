1ff78201a79d3a27058b7d496aa25ea93edcf311
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-16 10:16:18 +0800

    udpate herE

--- Playg4js/playg4.js
+++ Playg4js/playg4.js
@@ -95,10 +95,10 @@
 // Todo continue = skip the iteration of loop 
 
 
-for(let i = 1 ; i < 20 ; i += 1 ) {
-  // if (i == 13) {
-  //   break ;
-  // }
+for(let i = 1 ; i < 20 ; i -= 1 ) {
+  if (i == -13) {
+    break ;
+  }
   console.log(i)
 }
 
