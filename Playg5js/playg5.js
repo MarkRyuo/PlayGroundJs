aca224543250f884699069e94a72bdb18636251d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-21 07:35:07 +0800

    update herE

--- Playg5js/playg5.js
+++ Playg5js/playg5.js
@@ -27,7 +27,9 @@ let column = 10 ;
 for(let i = 0 ; i <= row ; i++ ) {
   for(let j = 0 ; j <= column ; j++){
     column += j ;
-    console.log(column) ;
+    if(column >= 50 ){
+      break ;
+    }
   }
   row += i ;
   console.log(row) ;
