0c0ec5b23b7320c00ffbb25c6daabf6b9559910d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-21 07:20:12 +0800

    update herE

--- Playg5js/playg5.js
+++ Playg5js/playg5.js
@@ -9,11 +9,15 @@
 // console.log(z) ;
 
 
-for(let i = 0 ; i < 2 ; i+=1 ) { 
-  for(let j = 0 ; j < 4 ; j +=1 ) {
-    console.log(j) ;
-  }
-  // console.log(i);
-}
+// for(let i = 0 ; i < 2 ; i+=1 ) { 
+//   for(let j = 0 ; j < 4 ; j +=1 ) {
+//     console.log(j) ;
+//   }
+//   // console.log(i);
+// }
 
 // example neto is first for loop ay parang row tapos ang second loop naman ay column 
+
+// TODO 
+
+let random = Math.ceil(Math.ramdom()* 50 ) ;
