4b1ad1b8abedd8fa5211919fb4c60d3e31ec0697
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-23 21:08:58 +0800

    udpate herE

--- Playg5js/playg5.js
+++ Playg5js/playg5.js
@@ -149,6 +149,6 @@ function hypotenuse(sideA, sideB) {
 
 	sideC = Math.ceil(Math.sqrt(Math.pow(sideA, 2)) + Math.pow(sideB, 2)) ;
 
-	console.log("side A is ", sideA, "side B is ", sideB ,sideC) ;
+	console.log("side A is ", sideA, "side B is ", sideB, "the answer is " , sideC) ;
 
 }
