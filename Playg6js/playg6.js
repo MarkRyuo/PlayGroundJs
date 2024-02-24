b964fc0a33705537bfd8b5aeace3a0637ea19d69
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-24 08:15:23 +0800

    update herE

--- Playg6js/playg6.js
+++ Playg6js/playg6.js
@@ -1,7 +1,19 @@
 // Todo Function 
 
+naming() ;
+
+function naming() {
+
+  let userName = "Jhon Mark" ;
+  console.log("Hello and Welcome!")
+}
+
+
+
 Timebot() ;
 
 function Timebot() {
-  
+
+
+
 }
