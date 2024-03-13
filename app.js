8dbd05e889d52d942727015cd9a1b4fe353e1dd7
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-13 21:59:27 +0800

    uapety herE

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 
 // for i is like counter 
 
+// talkOf() ;
+
+// function talkOf(){
+//   for(let counter = 0 ; counter < 10 ; counter+=1 ) {
+//     console.log(counter) ;
+//   }
+// }
+
+
 talkOf() ;
 
 function talkOf(){
-  for(let counter = 0 ; counter < 10 ; counter+=1 ) {
-    console.log(counter) ;
+  for(let counterX = 0 ; counterX < 10 ; counterX+=1 ) {
+    for(let counterY = 2 ; counterY < 6 ; counterY+=2){
+      
+    }
   }
 }
