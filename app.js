2dae55c76b2496154253423290a02b82dd68abd5
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-15 08:24:27 +0800

    udapet herE

--- app.js
+++ app.js
@@ -4,5 +4,8 @@
 
 const nameOf = function () {
   let age = 40 ;
-  age >= 30 ? console.log("Your Old Enough") : console.log("Your Young")
+  
+  age >= 30 ? console.log("Your Old Enough") : console.log("Your Young") ;
 }
+
+nameOf() ;
