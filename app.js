26c8784c2ed54d88f5f0f9d811bd4090f72d503b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-15 09:04:30 +0800

    udpaet ehrE

--- app.js
+++ app.js
@@ -2,18 +2,24 @@
 
 // Function 
 
-const ageOf = function () {
-  let age = 21 ;
-  return age ;
-  // age >= 30 ? console.log("Your Old Enough") : console.log("Your Young") ;
+// const ageOf = function () {
+//   let age = 21 ;
+//   return age ;
+//   // age >= 30 ? console.log("Your Old Enough") : console.log("Your Young") ;
+// }
+
+// let age = ageOf() ;
+
+// let nameL = "Jhon Mark" ;
+// nameOf(nameL, age) ;
+
+// function nameOf(nameL, age){
+//   console.log(`Hello ${nameL}, your age is ${age} right? `) ;
+// }
+
+
+
+const userName = function() {
+  let user = "Jhon Mark" ;
+  return user ;
 }
-
-let age = ageOf() ;
-
-let nameL = "Jhon Mark" ;
-nameOf(nameL, age) ;
-
-function nameOf(nameL, age){
-  console.log(`Hello ${nameL}, your age is ${age} right? `) ;
-}
-
