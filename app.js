fb5e94e27f071f426e17ac38e23183a545e93f49
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-30 20:37:23 +0800

    updat ehere

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // console.log("Hello Program")
 
+// Create a function na mag sasay ng 4 times ng hello program then ito ay lalabas ng isang say anything 
+
 const SayHello = () => {
   let hello = "Hello Program"
   for(let i = 0 ; i < 10 ; i++) {
