bcb4cbbbe39fe60cbf6982c690e7aa1ca1696bde
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-30 20:22:04 +0800

    udpate herE

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello Program")
+// console.log("Hello Program")
+
+const SayHello = () => {
+  let hello = "Hello Program"
+  for(let i = 0 ; i = 10 ; i++) {
+  hello[i]
+  console.log(hello)
+  }
+}
