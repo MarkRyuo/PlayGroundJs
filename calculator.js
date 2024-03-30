835615b733ebc80c0147609c0d38727f270953e1
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-30 21:47:22 +0800

    udpat ehrE'

--- calculator.js
+++ calculator.js
@@ -1,18 +1,6 @@
 // Todo Create a simple calculator 
 
 
-
-const addition = (x, y) => {
-  
-  let result = x + y ;
-  return result ;
-  
-}
-
-let add = addition(x, y) ;
-console.log(add)
-
-
 const calculate = () => {
   const x = 10 ;
   const y = 40 ;
@@ -22,3 +10,14 @@ const calculate = () => {
 }
 
 calculate();
+
+const addition = (x, y) => {
+
+  let result = x + y ;
+  return result ;
+
+}
+
+let add = addition(x, y) ;
+console.log(add)
+
