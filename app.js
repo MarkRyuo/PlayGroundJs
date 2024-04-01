59a68c7c9a93967a28c572a67c5b35507bddd4d8
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-01 13:28:57 +0800

    updat ehrE

--- app.js
+++ app.js
@@ -2,16 +2,25 @@
 
 // Create a function na mag sasay ng 4 times ng hello program then ito ay lalabas ng isang say anything 
 
-const SayHello = () => {
-  let hello = "Hello Program"
-  for(let i = 0 ; i < 4 ; i++) {
-  hello[i]
-  console.log(hello)
-    if(hello) {
-      console.log("Hey, Say Something")
-    }
+// const SayHello = () => {
+//   let hello = "Hello Program"
+//   for(let i = 0 ; i < 4 ; i++) {
+//   hello[i]
+//   console.log(hello)
+//     if(hello) {
+//       console.log("Hey, Say Something")
+//     }
 
+//   }
+// }
+
+// SayHello()
+
+
+
+class Rectangle {
+  // To setup 
+  constructor() {
+    console.log("The rectangle is now created")
   }
 }
-
-SayHello()
