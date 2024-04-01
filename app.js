765f030e1b5ad57e8d72e9d03d4c1a96cbc38145
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-01 14:08:17 +0800

    update here

--- app.js
+++ app.js
@@ -27,22 +27,32 @@
 
 // let rec = new Rectangle() ;
 
-class Username {
-  // setup
-  constructor(name, age) {
+// class Username {
+//   // setup
+//   constructor(name, age) {
 
-    this.name = name ;
-    this.age = age ;
-  }
+//     this.name = name ;
+//     this.age = age ;
+//   }
 
-  // Function 
-  username() {
-    console.log(`Your name is: ${this.name}`)
-    console.log(`Your age is: ${this.age}`)
-  }
+//   // Function 
+//   username() {
+//     console.log(`Your name is: ${this.name}`)
+//     console.log(`Your age is: ${this.age}`)
+//   }
   
+// }
+
+// const person1 = new Username("Jhon Mark", 21) ;
+
+// person1.username() ;
+
+
+// Todo Class is like a Blue Print of the house 
+
+class House {
+
+  constructor (color) {
+    this.color = color
+  }
 }
-
-const person1 = new Username("Jhon Mark", 21) ;
-
-person1.username() ;
